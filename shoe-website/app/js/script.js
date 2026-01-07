7ab9143013f314b2884ccf905a08e317a61548bc
Pikachu <nguyenha954@gmail.com>
2026-01-07 17:42:19 +0700

    sixth commit: Register page-html

--- shoe-website/app/js/script.js
+++ shoe-website/app/js/script.js
@@ -1,1 +1,53 @@
-console.log('Hello World');
+const form = document.getElementById("login");
+const email = document.getElementById("email-Login");
+const password = document.getElementById("password-Login");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validationInput();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validationInput = () => {
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === "" || emailValue === null) {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Provide a valid email address");
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordValue === "") {
+    setError(password, "Password is required");
+  } else if (passwordValue.length < 8 && passwordValue.length > 20) {
+    setError(password, "Password must be at least 8 to 20 character.");
+  } else {
+    setSuccess(password);
+  }
+};
